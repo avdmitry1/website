04e7dfcf608c3b98a555c78cef95a9303add6881
avdmitry <avdmitry1@gmail.com>
2025-01-16 12:27:58 +0200

    Some new updates

--- website/main/views.py
+++ website/main/views.py
@@ -13,6 +13,26 @@ def artists(request):
     return render(request, "main/artists.html")
 
 
+def artists_view(request):
+    artists = [
+        {"name": "Artist 1", "image": "img/font1.png"},
+        {"name": "Artist 2", "image": "img/font2.png"},
+        {"name": "Artist 3", "image": "img/font3.png"},
+        {"name": "Artist 4", "image": "img/font4.png"},
+        {"name": "Artist 5", "image": "img/font5.png"},
+        {"name": "Artist 6", "image": "img/font6.png"},
+        {"name": "Artist 3", "image": "img/font3.png"},
+        {"name": "Artist 6", "image": "img/font6.png"},
+        {"name": "Artist 4", "image": "img/font4.png"},
+        {"name": "Artist 5", "image": "img/font5.png"},
+        {"name": "Artist 6", "image": "img/font6.png"},
+        {"name": "Artist 3", "image": "img/font3.png"},
+        {"name": "Artist 6", "image": "img/font6.png"},
+        
+    ]
+    return render(request, "main/artists.html", {"artists": artists})
+
+
 def events(request):
     return render(request, "main/events.html")
 
